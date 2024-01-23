afcf55332f67471c07010b8af996da428db4036f
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-23 23:11:05 +0900

    구현 : MemberMessageGroup entity(Member -MessageGroup 중간 연결 역할)

---
+++ src/main/java/com/example/livechat/domain/entity/MemberMessageGroup.java
@@ -0,0 +1,26 @@
+package com.example.livechat.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(schema = "live_chat")
+public class MemberMessageGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "message_group_id")
+    private MessageGroup messageGroup;
+
+}
