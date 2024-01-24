a023d427d00a64a573e1ee4893b227fea6e764f7
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-24 23:26:09 +0900

    수정 : 바인딩 @RequestBody 빠짐 추가, username 중복 체크

--- src/main/java/com/example/livechat/controller/api/MemberApiController.java
+++ src/main/java/com/example/livechat/controller/api/MemberApiController.java
@@ -6,11 +6,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +23,15 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<?> saveMember(@Valid MemberSaveDto memberSaveDto,
+    public ResponseEntity<?> saveMember(@RequestBody @Valid MemberSaveDto memberSaveDto,
                                         BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             ObjectError error = bindingResult.getAllErrors().get(0);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+
+        if (memberService.duplicateUsernameCheck(memberSaveDto.getUsername())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("중복 username이 있습니다.");
         }
 
         memberService.saveMember(memberSaveDto);
