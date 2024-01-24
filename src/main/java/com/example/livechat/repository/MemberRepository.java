a838b56e8cbba496796a543a7387357c74ea36f1
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-24 23:25:16 +0900

    구현 : username으로 Member 검색

--- src/main/java/com/example/livechat/repository/MemberRepository.java
+++ src/main/java/com/example/livechat/repository/MemberRepository.java
@@ -2,7 +2,12 @@ package com.example.livechat.repository;
 
 import com.example.livechat.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 
 }
