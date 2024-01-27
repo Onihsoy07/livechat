639a7bf4572253a786d38d3fdcdef3f739c4a7d8
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-27 23:46:27 +0900

    구현 : 로그인 확인 컨트롤러

---
+++ src/main/java/com/example/livechat/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.livechat.controller;
+
+import com.example.livechat.domain.dto.MemberDto;
+import com.example.livechat.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/{username}")
+    public String memberChatHome(@PathVariable(name = "username") final String username,
+                                 Model model) {
+        MemberDto memberDto = memberService.getMember(username);
+        model.addAttribute("memberDto" ,memberDto);
+        return "chat/home";
+    }
+}
