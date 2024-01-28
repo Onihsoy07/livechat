899433b4adf0a243cb9fb254b2920ef82abb3bd6
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-28 23:10:39 +0900

    로그인 사용자 객체 생성

---
+++ src/main/java/com/example/livechat/auth/PrincipalDetails.java
@@ -0,0 +1,58 @@
+package com.example.livechat.auth;
+
+import com.example.livechat.domain.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private Member member;
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(() -> {
+            return "ROLE_" + member.getRole();
+        });
+        return auth;
+    }
+}
