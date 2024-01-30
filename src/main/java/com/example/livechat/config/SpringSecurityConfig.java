446279e0bdd0bfb890b268f208a700611cce98a3
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-30 23:57:20 +0900

    Spring Security 모든 접근 허용(Vue 프론트는 호스트가 달라서? 403 걸림)

--- src/main/java/com/example/livechat/config/SpringSecurityConfig.java
+++ src/main/java/com/example/livechat/config/SpringSecurityConfig.java
@@ -1,20 +1,25 @@
 package com.example.livechat.config;
 
+import com.example.livechat.auth.JwtProvider;
+import com.example.livechat.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -35,9 +40,17 @@ public class SpringSecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> {
                     authorize
-                            .requestMatchers("/", "/auth").permitAll()
-                            .anyRequest().authenticated();
+//                            .requestMatchers("/", "/auth").permitAll()
+                            .anyRequest().permitAll();
                 })
+                .addFilterBefore(
+                        new JwtFilter(jwtProvider),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+//                 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .build();
     }
 
--- src/main/java/com/example/livechat/controller/api/MemberApiController.java
+++ src/main/java/com/example/livechat/controller/api/MemberApiController.java
@@ -3,16 +3,14 @@ package com.example.livechat.controller.api;
 import com.example.livechat.domain.dto.MemberSaveDto;
 import com.example.livechat.domain.entity.HttpResponseDto;
 import com.example.livechat.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -23,8 +21,13 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping
-    public HttpResponseDto<?> saveMember(@RequestBody @Valid MemberSaveDto memberSaveDto,
-                                        BindingResult bindingResult) {
+    public HttpResponseDto<?> saveMember(HttpServletRequest request,
+                                         @RequestBody @Valid MemberSaveDto memberSaveDto,
+                                         BindingResult bindingResult) {
+        //Vue 프론트에서 요청 시 클라이언트 URL 확인(0:0:0:0:0:0:0:1)
+        //Spring Security authorizeHttpRequests에 host가 달라서 안걸림?
+        String requestUrl = request.getRemoteAddr().toString();
+
         if (bindingResult.hasErrors()) {
             ObjectError error = bindingResult.getAllErrors().get(0);
             return new HttpResponseDto<>(HttpStatus.BAD_REQUEST.value(), false, "Valid error", error);
