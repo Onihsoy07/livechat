1962f5a81c30b2c0f8400ec0c6fdb8a6c661f449
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-01-31 23:45:39 +0900

    RangeError: Maximum call stack size exceeded(vue-router, axiox ) 에러?

--- src/main/resources/front/src/main.js
+++ src/main/resources/front/src/main.js
@@ -1,11 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import Axios from 'axios'
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Axios from 'axios';
 
 createApp(App)
     .use(VueRouter)
     .use(Axios)
     .mount('#app')
-
-App.config.globalProperties.axios = Axios;
