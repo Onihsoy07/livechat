d14cc9cab45402a3f35de0fbd82f6a7b2e43d155
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-01 23:41:15 +0900

    Vue store 추가 및 기본 세팅

--- src/main/resources/front/src/main.js
+++ src/main/resources/front/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import store from './store';
 import Axios from 'axios';
 
 createApp(App)
     .use(VueRouter)
+    .use(store)
     .use(Axios)
     .mount('#app')
