95c23494cd42c3fc0ebb9b45870a07c9e614d1f6
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-02 23:58:06 +0900

    JWT secret_key 수정(길이 부족으로 jwt 생성 시 에러)

--- src/main/java/com/example/livechat/domain/constant/JwtConst.java
+++ src/main/java/com/example/livechat/domain/constant/JwtConst.java
@@ -3,7 +3,7 @@ package com.example.livechat.domain.constant;
 public interface JwtConst {
 
     // HS256 (대칭키)
-    public static final String SECRET = "E7P4Sc74oXskc9atAjFm32hb1H7fe4cHT7IjOkVlZmU58Er8a9v7CIdw9vRXRq";
+    public static final String SECRET = "E7P4Sc74oXsklSf+UZL1LiKLHkGfYc9atAjFm32hb1H7SiFToLh/Pc5Rfe4cHT7IjOkVlZmU58Er8a9v7CIdw9vR5RV8fXRq";
     // 일주일
     public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;
     public static final String TOKEN_PREFIX = "Bearer ";
