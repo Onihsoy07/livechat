defa125bac916ba6f1ced34d29672d231bf22bf8
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-02 23:59:01 +0900

    프론트 로그인 컴포넌트 구현

---
+++ src/main/resources/front/src/components/AuthLogin.vue
@@ -0,0 +1,53 @@
+<template>
+    <h1>로그인</h1>
+    <input type="text" name="username" id="username" placeholder="username" v-model="username" />
+    <input type="password" name="password" id="password" placeholder="password" v-model="password" />
+    <button @click="login">로그인</button>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const store = useStore();
+const router = useRouter();
+let username = ref('');
+let password = ref('');
+
+
+const login = () => {
+    axios({
+        method: 'post',
+        url: 'http://localhost:8080/api/auth/login', 
+        data: JSON.stringify({
+            username: username.value,
+            password: password.value,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        if (res.data.success) {
+            console.log(res);
+            // store.commit('SET_TOKEN', res.data.data.token);
+            window.localStorage.setItem('token', res.data.data.token);
+            store.commit('SET_LOGIN');
+            router.push('/');
+        } else {
+            console.log(res.data);
+            alert(res.data.message);
+        }
+    }).catch((error) => {
+        console.log(error);
+        username = ref('');
+        password = ref('');
+        alert(error.response.data.message);
+    });
+}
+</script>
+
+<style scoped>
+
+</style>
