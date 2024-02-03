122c4f72ef13b3675823112ca9bd1d61ff7f26ae
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-03 21:36:57 +0900

    프론트 메인화면(그냥 데이터 확인용 추가)

--- src/main/resources/front/src/components/MainHome.vue
+++ src/main/resources/front/src/components/MainHome.vue
@@ -1,9 +1,19 @@
 <template>
     <h1>HOME</h1>
+    <div>
+        <div>{{ isLogin }}</div>
+        <div>{{ username }}</div>
+    </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 
+const store = useStore();
+
+const isLogin = computed(() => store.state.isLogin);
+const username = computed(() => store.state.username);
 </script>
 
 <style scoped>
