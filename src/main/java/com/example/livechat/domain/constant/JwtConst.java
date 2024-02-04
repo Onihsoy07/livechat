5b7f57baa53581f6618b1c2eb854702192120ddd
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-04 22:37:55 +0900

    JWT 인증 헤더 이름 수정

--- src/main/java/com/example/livechat/domain/constant/JwtConst.java
+++ src/main/java/com/example/livechat/domain/constant/JwtConst.java
@@ -7,6 +7,6 @@ public interface JwtConst {
     // 일주일
     public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER = "Authorization";
+    public static final String HEADER = "Authentication";
 
 }
