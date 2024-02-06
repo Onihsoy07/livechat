c1cd49084e5a565d2a4e805870beaeab6f034311
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-06 23:53:51 +0900

    대화방 검색 기능

--- src/main/java/com/example/livechat/service/MessageGroupService.java
+++ src/main/java/com/example/livechat/service/MessageGroupService.java
@@ -1,5 +1,6 @@
 package com.example.livechat.service;
 
+import com.example.livechat.domain.dto.MessageGroupDto;
 import com.example.livechat.domain.dto.MessageGroupSaveDto;
 import com.example.livechat.domain.entity.MessageGroup;
 import com.example.livechat.repository.MessageGroupRepository;
@@ -7,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -23,6 +27,17 @@ public class MessageGroupService {
                 .build();
 
         return messageGroupRepository.save(messageGroup);
+    }
+
+    public List<MessageGroupDto> searchChatName(String chatName) {
+        List<MessageGroup> messageGroupList = messageGroupRepository.findByMessageGroupNameContaining(chatName);
+        List<MessageGroupDto> messageGroupDtoList = new ArrayList<>();
+
+        for (MessageGroup messageGroup : messageGroupList) {
+            messageGroupDtoList.add(new MessageGroupDto(messageGroup));
+        }
+
+        return messageGroupDtoList;
     }
 
 }
