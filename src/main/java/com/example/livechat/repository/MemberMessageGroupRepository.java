83b30816674fc5d3c384afa38eaea428d04053e2
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-07 23:42:39 +0900

    유저 대화방 찾는 sql N+1 문제 해결(fetch join 추가)

--- src/main/java/com/example/livechat/repository/MemberMessageGroupRepository.java
+++ src/main/java/com/example/livechat/repository/MemberMessageGroupRepository.java
@@ -2,11 +2,14 @@ package com.example.livechat.repository;
 
 import com.example.livechat.domain.entity.MemberMessageGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberMessageGroupRepository extends JpaRepository<MemberMessageGroup, Long> {
 
-    List<MemberMessageGroup> findByMember_Username(String username);
+    @Query("select mmg from MemberMessageGroup mmg join fetch mmg.messageGroup where mmg.member.username like concat('%', :username, '%')")
+    List<MemberMessageGroup> findByMember_Username(@Param("username") String username);
 
 }
