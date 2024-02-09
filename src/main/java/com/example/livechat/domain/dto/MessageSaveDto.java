8882cc84c97986f47490f690d1172a2ac4bcc62c
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-09 10:14:29 +0900

    MessageSaveDto 구현

---
+++ src/main/java/com/example/livechat/domain/dto/MessageSaveDto.java
@@ -0,0 +1,20 @@
+package com.example.livechat.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageSaveDto {
+
+    @NotNull(message = "대화방이 존재하지 않습니다.")
+    private Long chatId;
+
+    @NotBlank(message = "대화 내용이 존재하지 않습니다.")
+    private String message;
+
+}
