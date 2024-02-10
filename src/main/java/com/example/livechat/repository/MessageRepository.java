7a173b635a8f6f684c3ea04c0106a34df3049b09
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-10 23:55:17 +0900

    대화방 메시지 리스트 탐색 기능(JOIN FETCH 추가)

--- src/main/java/com/example/livechat/repository/MessageRepository.java
+++ src/main/java/com/example/livechat/repository/MessageRepository.java
@@ -2,7 +2,14 @@ package com.example.livechat.repository;
 
 import com.example.livechat.domain.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query("select m from Message m join fetch m.sender where m.messageGroup.id = :chatId")
+    List<Message> findByMessageGroup_Id(@Param("chatId") Long chatId);
 
 }
--- src/main/java/com/example/livechat/service/MessageService.java
+++ src/main/java/com/example/livechat/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.example.livechat.service;
 
+import com.example.livechat.domain.dto.MessageDto;
 import com.example.livechat.domain.dto.MessageSaveDto;
 import com.example.livechat.domain.entity.Member;
 import com.example.livechat.domain.entity.Message;
@@ -9,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -30,4 +34,17 @@ public class MessageService {
 
         messageRepository.save(message);
     }
+
+    @Transactional(readOnly = true)
+    public List<MessageDto> getChatMessageList(Long chatId) {
+        List<Message> messageList = messageRepository.findByMessageGroup_Id(chatId);
+        List<MessageDto> messageDtoList = new ArrayList<>();
+
+        for (Message message : messageList) {
+            messageDtoList.add(new MessageDto(message));
+        }
+
+        return messageDtoList;
+    }
+
 }
