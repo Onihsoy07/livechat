6fd83c4a490823301e78d04e04ce7ad8e4646559
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-12 23:32:07 +0900

    WebSocketConfig 수정

--- src/main/java/com/example/livechat/config/WebSocketConfig.java
+++ src/main/java/com/example/livechat/config/WebSocketConfig.java
@@ -8,29 +8,29 @@ import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+//@EnableWebSocket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketHandler webSocketHandler;
+//    private final WebSocketHandler webSocketHandler;
+
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+//    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws/chat")
-//                .setAllowedOriginPatterns("*")
-//                .withSockJS();
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        // 서버 -> 클라이언트 메시지 endpoint
-//        registry.enableSimpleBroker("/sub");
-//
-//        // 클라이언트 -> 서버 메시지 endpoint
-//        registry.setApplicationDestinationPrefixes("/pub");
-//    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 서버 -> 클라이언트 메시지 endpoint
+        registry.enableSimpleBroker("/sub");
+
+        // 클라이언트 -> 서버 메시지 endpoint
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
 }
