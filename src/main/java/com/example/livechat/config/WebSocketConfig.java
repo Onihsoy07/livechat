e7087a2c5230a1d2399d5cbacc9f12f8e4c01d2f
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-12 15:49:24 +0900

    웹소켓 동작 기능 구현(동작 확인 완료)

--- src/main/java/com/example/livechat/config/WebSocketConfig.java
+++ src/main/java/com/example/livechat/config/WebSocketConfig.java
@@ -9,28 +9,28 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
 
-//    private final WebSocketHandler webSocketHandler;
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+    }
+
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws/chat")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
+//    }
 //
 //    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        // 서버 -> 클라이언트 메시지 endpoint
+//        registry.enableSimpleBroker("/sub");
+//
+//        // 클라이언트 -> 서버 메시지 endpoint
+//        registry.setApplicationDestinationPrefixes("/pub");
 //    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 서버 -> 클라이언트 메시지 endpoint
-        registry.enableSimpleBroker("/sub");
-
-        // 클라이언트 -> 서버 메시지 endpoint
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
 }
--- src/main/java/com/example/livechat/handler/WebSocketHandler.java
+++ src/main/java/com/example/livechat/handler/WebSocketHandler.java
@@ -1,6 +1,7 @@
 package com.example.livechat.handler;
 
 import com.example.livechat.domain.dto.MessageDto;
+import com.example.livechat.domain.dto.MessageSaveDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -30,12 +32,22 @@ public class WebSocketHandler extends TextWebSocketHandler {
         String payload = message.getPayload();
         log.info("WS payload : {}", payload);
 
-        MessageDto messageDto = mapper.readValue(payload, MessageDto.class);
+        MessageSaveDto messageDto = mapper.readValue(payload, MessageSaveDto.class);
         log.info("WS messageDto : {}", messageDto);
 
-        // chatSessionMap Value 값에 넣을 데이터는?
-        // request body에 chatId있음. 
-        // 여기는 컨트롤러 전에 걸리는가? 확인 필요
+        Long chatId = messageDto.getChatId();
+        if (!chatSessionMap.containsKey(chatId)) {
+            chatSessionMap.put(chatId, new HashSet<>());
+        }
+        Set<WebSocketSession> chatWebSocketSessions = chatSessionMap.get(chatId);
+
+        chatWebSocketSessions.add(session);
+
+        removeClosedSession(chatWebSocketSessions);
+
+        sendMessageToChat(messageDto, chatWebSocketSessions);
+
+        log.info("대화방ID {}, 접속 세션 수 : {}", chatId, chatWebSocketSessions.size());
     }
 
     // 클라이언트 접속 시 호출 메서드
@@ -43,6 +55,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("{} 연결됨", session.getId());
         sessions.add(session);
+        log.info("연결 세션 수 : {}", sessions.size());
     }
 
     // 클라이언트 접속 해제 시 호출 메서드
@@ -50,6 +63,28 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info("{} 연결 끊김", session.getId());
         sessions.remove(session);
+        log.info("연결 세션 수 : {}", sessions.size());
+    }
+
+    // close 된 세션 제거
+    private void removeClosedSession(Set<WebSocketSession> chatRoomSession) {
+        chatRoomSession.removeIf(sess -> !sessions.contains(sess));
+    }
+
+    private void sendMessageToChat(MessageSaveDto messageSaveDto, Set<WebSocketSession> chatWebSocketSessions) {
+        for (WebSocketSession chatWebSocketSession : chatWebSocketSessions) {
+            sendMessage(chatWebSocketSession, messageSaveDto);
+        }
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error("WS IOException", e);
+        } catch (IllegalStateException e) {
+            throw e;
+        }
     }
 
 }
