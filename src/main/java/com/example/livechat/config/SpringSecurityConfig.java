973b116468ee318392a1843a0b564da977a81b63
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-02-28 22:42:04 +0900

    설정 : Spring Security AuthenticationManager 빈

--- src/main/java/com/example/livechat/config/SpringSecurityConfig.java
+++ src/main/java/com/example/livechat/config/SpringSecurityConfig.java
@@ -7,6 +7,8 @@ import com.example.livechat.handler.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -63,6 +65,11 @@ public class SpringSecurityConfig {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
